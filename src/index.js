0ab88dbd02198fd75463db786ac4b5b4c7825bcf
Shikhar Yadav <shikharyadav645@gmail.com>
2024-10-13 00:22:11 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 // require('dotenv').config({path : './env'})
 import dotenv from 'dotenv'
 import connectDB from "./db/index.js";
+import { app } from './app.js';
+
 
 dotenv.config({
-    path : './env'
+    path : './.env'
 })
 
+const port = process.env.PORT ;
+
 connectDB()
+.then(() => {
+    app.on('error',(error) => {
+        console.error("ERROR AT CONNECTING APP !! :",error);
+       
+    })
+    app.listen(port,() => {
+        console.log(`SUCESSFULLY LISTENING AT : ${port}`);
+    })
+})
+.catch((error) =>{
+    console.log("DATABASE CONNECTION FAILED !! :",error)
+})
+
 
 
 
